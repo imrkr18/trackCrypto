f79d41bbbc1bad5c157d36de4931f90272ede7ba
imrkr18 <kuldipgohil1811@gmail.com>
2024-08-21 01:08:30 +0530

    code added for track crypto

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>
+                Copyright @2024, TrackCrypto - All Rights Reserved.
+            </p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import './Navbar.css'
+import crpytoLogo from '../../assets/trackCrypto.png'
+import { useContext } from 'react'
+import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currecyHandler = (e) => {
+    switch (e.target.value) {
+      case "usd":
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      case "inr":
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      default:
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+    }
+  };
+  return (
+    <div className='navbar'>
+      <Link to={'/'}>
+        <img src={crpytoLogo} className='logo' alt="cryptoLogo" />
+      </Link>
+      <ul>
+        <Link to={'/'}><li>Home</li></Link>
+        <li>Featuers</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className='nav-right'>
+        <select onChange={currecyHandler}>
+          <option value="usd">USD</option>
+          <option value="inr">INR</option>
+        </select>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
